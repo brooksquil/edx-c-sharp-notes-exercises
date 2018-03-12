07807d17c0ed6a64128908d7050d66699bd6a469
Brooke Wittenberg <brooke.wittenberg@gmail.com>
2018-03-12 00:21:46 -0500

    finished exercise

---
+++ mod2/mod2-exercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace mod2_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 9; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine("XOXOXOXO");
+                }
+                else
+                {
+                    Console.WriteLine("OXOXOXOX");
+                }
+            }
+        }
+    }
+}
